21b4014776fdb99523bf396b956682cad29befd7
Mohan Liu <mohan.liu.personal@gmail.com>
2016-11-01 23:51:03 -0400

    Initial Commit

---
+++ myfirstsstupidmacapp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  myfirstsstupidmacapp
+//
+//  Created by Mohan on 01/11/2016.
+//
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
